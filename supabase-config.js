89f7522613b1aa6e003cbfc267bd59d247076790
mohammadrezasamieizadeh-glitch <mohammadrezasamieizadeh@gmail.com>
2026-08-22 23:37:29 +0330

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-// کلید publishable/anon برای مرورگر قابل استفاده است؛ امنیت با RLS در Supabase اعمال می شود.
-window.SUPABASE_URL = "https://lvmhgiwbdlqeahmwculr.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_MgVd7Kxkst6TgfjR5Mgp3g_gosERxE6";
