e694004a6f3a90ec1c8687c9595b71962a458cd6
mohammadrezasamieizadeh-glitch <mohammadrezasamieizadeh@gmail.com>
2026-08-22 22:41:43 +0330

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// اطلاعات پروژه Supabase را اینجا وارد کن.
-// این دو مقدار را از Supabase > Project Settings > API می‌گیری.
-// anon key را می‌توان در سایت عمومی قرار داد؛ امنیت اصلی با RLS در دیتابیس انجام می‌شود.
-window.SUPABASE_URL = "https://lvmhgiwbdlqeahmwculr.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_MgVd7Kxkst6TgfjR5Mgp3g_gosERxE6";
